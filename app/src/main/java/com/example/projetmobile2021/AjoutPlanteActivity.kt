5ee317c3f19723235aba5660eef6777d15ce82ab
KACEM CHAOUCHE nicolas <kacem.nicolas@gmail.com>
2021-12-02 16:27:45 +0100

    Ajout Activity + data class plante

---
+++ app/src/main/java/com/example/projetmobile2021/AjoutPlanteActivity.kt
@@ -0,0 +1,23 @@
+package com.example.projetmobile2021
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.projetmobile2021.databinding.ActivityAjoutPlanteBinding
+
+class AjoutPlanteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ajout_plante)
+
+    }
+
+
+
+}
