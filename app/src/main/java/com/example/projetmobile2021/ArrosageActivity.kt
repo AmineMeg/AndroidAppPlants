778413a9b0800dce3bb0edf480fc99ac83806fbc
MEGUENNI amine <meguenni@informatique.univ-paris-diderot.fr>
2022-01-07 15:22:55 +0100

    mise a jour frequence arrosage

--- app/src/main/java/com/example/projetmobile2021/ArrosageActivity.kt
+++ app/src/main/java/com/example/projetmobile2021/ArrosageActivity.kt
@@ -4,30 +4,33 @@ import android.annotation.SuppressLint
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
-import androidx.annotation.ContentView
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.projetmobile2021.databinding.ActivityArrosageBinding
 import java.time.LocalDate
 
 class ArrosageActivity : AppCompatActivity() {
 
     val model by lazy { ViewModelProvider(this).get(PlanteViewModel::class.java)}
     var plantes = MutableLiveData<List<Plante>>()
+    private lateinit var binding: ActivityArrosageBinding
+
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arrosage)
 
+        binding = ActivityArrosageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val recyclerView = findViewById(R.id.recycler) as RecyclerView
-        val adapter = ArrosageAdapter(this)
+
+        binding.recycler
+
+        val adapter = ArrosageAdapter(this, this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
         model.getPlante()
